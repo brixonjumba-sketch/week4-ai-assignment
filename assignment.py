fa4d02632e30583f88a51e9fefca64d5edbdab77
brixonjumba-sketch <brixonjumba@gmail.com>
2026-03-13 21:39:27 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,27 @@
+# Question 4
+print("Hello AI Students")  
+
+
+
+# Question 5 
+name = "John"
+age = 20
+city = "Nairobi"
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+
+
+# Question 6
+programming_languages = ["Python", "Java", "C++", "JavaScript"]
+print(f"Programming Languages: {programming_languages[0]}")
+
+
+# Question 7
+student = {
+"name": "John",
+"age": 21,
+"course": "AI"
+}
+print(f"Student Name: {student['name']}")
+
